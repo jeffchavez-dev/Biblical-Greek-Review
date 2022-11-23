32ddb6b9a589174230dae285f13f7357d19afd1f
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-11-23 14:06:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -640,7 +640,7 @@ allVocabs.addEventListener('click', () => {
     
     const allVocabsEl = document.querySelector('.all-vocabsEl')
     lessonsEL.classList.add('hide')
-    allVocabList.push(...lessonOne, ...lessonTwo, ...lessonThree, ...lessonFour, ...lessonFive, ...lessonSix, ...lessonSeven, ...lessonEight, ...lessonNine, ...lessonTen, ...lessonEleven);
+    allVocabList.push(...lessonOne, ...lessonTwo, ...lessonThree, ...lessonFour, ...lessonFive, ...lessonSix, ...lessonSeven, ...lessonEight, ...lessonNine, ...lessonTen, ...lessonEleven ...lessonTwelve);
 
     let vocabularyList = allVocabList.map(vocab => {
         return `
