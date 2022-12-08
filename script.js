36ef06688dc0f667bf679a72017e84401382d0d1
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-12-08 19:51:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1418,6 +1418,21 @@ otherParadigmsTable.innerHTML = otherParadigmEl;
         "1p-translation" : "We received were written",
         "2p-translation" : "You (all) were written",
         "3p-translation" : "They received were written"
+    },
+    {
+        "Paradigm": "Liquid Futures",
+        "1s" : "μενῶ",
+        "2s" : "μενεῖς",
+        "3s" : "ἐγράφη",
+        "1p" : "ἐγραφημεν",
+        "2p" : "ἐγραφητε",
+        "3p" : "ἐγραφησαν",
+        "1s-translation" : "I was written",
+        "2s-translation" : "You were written",
+        "3s-translation" : "He/she/it was written",
+        "1p-translation" : "We received were written",
+        "2p-translation" : "You (all) were written",
+        "3p-translation" : "They received were written"
     }
   ]
 
