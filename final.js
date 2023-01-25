07083b1855ec18d8ba664d5a65869d80c1f81e39
Jeff Chavez <jeff.chavez0828@gmail.com>
2023-01-26 06:17:57 +0800

    updated exams

--- final.js
+++ final.js
@@ -3,6 +3,8 @@
 
 const startTime = document.getElementById('start-time');
 const examBtn = document.getElementById('final-exam');
+const examPage = document.querySelector('.final-exam-page');
+const examEl = document.querySelector('.take-exam');
 
 
 
@@ -15,7 +17,20 @@ let start
 
 examBtn.addEventListener('click', ()=> {
     startTime.textContent = time;
-    prompt("Are you sure that you're ready?")
+
+    // const ready = document.createElement('div')
+    // ready.textContent = "Are you ready?"
+    // examEl.style.display = "none"
+    // examPage.appendChild(ready)
+
+    let ready = prompt("Are you sure that you're ready? Type YES, if you are!");
+
+    if (prompt != null) {
+        console.log("YES")
+
+    } else {
+        window.reload()
+    }
 
 })
 
