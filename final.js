eb976ed8916f5403f66b60749c5ca78b3e259276
Jeff Chavez <jeff.chavez0828@gmail.com>
2023-01-28 22:36:00 +0800

    Update final.js

--- final.js
+++ final.js
@@ -18,10 +18,10 @@ let start
 examBtn.addEventListener('click', ()=> {
     startTime.textContent = time;
 
-    // const ready = document.createElement('div')
-    // ready.textContent = "Are you ready?"
-    // examEl.style.display = "none"
-    // examPage.appendChild(ready)
+    const ready = document.createElement('div')
+    ready.textContent = "Are you ready?"
+    examEl.style.display = "none"
+    examPage.appendChild(ready)
 
     let ready = prompt("Are you sure that you're ready? Type YES, if you are!");
 
